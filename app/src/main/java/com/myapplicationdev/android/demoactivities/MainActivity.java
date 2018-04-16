e1aae5220096661c25ddd02e45b07826d495f929
shawnwong65 <16022774@myrp.edu.sg>
2018-04-16 15:34:00 +0800

    Added intent

--- app/src/main/java/com/myapplicationdev/android/demoactivities/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoactivities/MainActivity.java
@@ -1,7 +1,11 @@
 package com.myapplicationdev.android.demoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnDone = (Button) findViewById(R.id.buttonDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etName = (EditText) findViewById(R.id.editTextName);
+                EditText etAge = (EditText) findViewById(R.id.editTextAge);
+
+                String[] info = {etName.getText().toString(), etAge.getText().toString()};
+
+                Intent i = new Intent(MainActivity.this, DemoActivities2.class);
+
+                i.putExtra("info", info);
+
+                startActivity(i);
+            }
+        });
     }
 }
